26e25557d46b4804c9e2f4b42855146d15d0787b
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-01 12:14:30 +0200

    build:

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]      # contient les 3 premières lettres
+word_last_2 = word[-2:]      # contient les 2 dernières lettres
+middle_word = word[1:-1]     # contient le mot sans la première ni la dernière lettre
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
