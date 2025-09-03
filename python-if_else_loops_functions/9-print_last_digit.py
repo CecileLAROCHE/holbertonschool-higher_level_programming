0f312b5960adbbb0e6d07bc88f7a200d4502dc8d
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-03 17:41:54 +0200

    fix(création task 9): :bug:

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -6,4 +6,5 @@ def print_last_digit(number):
         last_digit = number % 10
     else:
         last_digit = -(abs(number) % 10)
-    print(last_digit, end='') 
+    print(last_digit, end='')
+    return last_digit
