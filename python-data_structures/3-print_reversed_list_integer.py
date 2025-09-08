704c4a245bb946c62143d5497196b3be381f6773
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-08 12:31:25 +0200

    fix(Python - Data Structures: Lists, Tuples): :bug: task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for nombre in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(nombre))
-        
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
