6ed5726120e97c7351b0f4b4e564cc32c12ea80c
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-09 10:08:49 +0200

    build(Python - Data Structures: Lists, Tuples): :hammer: creation task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for j in range(len(line)):
+            if j < len(line) - 1:
+                print("{:d}".format(line[j]), end=" ")
+            else:
+                print("{:d}".format(line[j]), end="")
+        print()
