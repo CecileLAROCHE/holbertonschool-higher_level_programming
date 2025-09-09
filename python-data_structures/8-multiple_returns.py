43ae8ad10c5af7274d4d2b1bf961ff059fd94f44
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-09 14:25:14 +0200

    fix(Python - Data Structures: Lists, Tuples): :bug: Pycodestyle validation

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    
+
     lenght = len(sentence)
     if lenght == 0:
         return (0, None)
     else:
-        return (lenght , sentence[0])
-  
+        return (lenght, sentence[0])
