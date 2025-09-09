ec24d19cf19fd391da0dbb50c953778e6b73af2b
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-09 14:23:24 +0200

    build(Python - Data Structures: Lists, Tuples): :hammer: task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def multiple_returns(sentence):
+    
+    lenght = len(sentence)
+    if lenght == 0:
+        return (0, None)
+    else:
+        return (lenght , sentence[0])
+  
