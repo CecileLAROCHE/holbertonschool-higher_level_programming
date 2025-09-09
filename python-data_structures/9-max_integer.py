d277d6ddba3d0a5574aa28a57a2a0a8a37161c68
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-09 14:34:09 +0200

    build(Python - Data Structures: Lists, Tuples): :hammer: task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        biggest = my_list[0]
+        for nb in my_list:
+            if nb > biggest:
+                biggest = nb
+    return biggest
+        
