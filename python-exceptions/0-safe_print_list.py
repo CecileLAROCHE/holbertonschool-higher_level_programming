080e62d82d0a8573023bd9f3c28bb5ea3442b53d
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-10 19:10:23 +0200

    fix(Python - Exceptions): :bug:

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             compteur = compteur + 1
-        except:
+        except IndexError:
             break
+
     print("")
     return compteur
