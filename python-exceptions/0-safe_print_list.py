61dc1a8283401f77028397bbd8ae5a1f7c271e87
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-10 19:07:48 +0200

    build(Python - Exceptions): :hammer: task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	
+    compteur = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            compteur = compteur + 1
+        except:
+            break
+    print("")
+    return compteur
