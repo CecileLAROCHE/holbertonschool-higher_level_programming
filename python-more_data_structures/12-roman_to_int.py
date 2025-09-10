2d4fc3a7f4df7fe75716c3cb2aa79a96afff524a
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-10 18:27:14 +0200

    fix(Python - More Data Structures: Set, Dictionary): :bug:

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,6 +9,7 @@ def roman_to_int(roman_string):
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
     while i < len(roman_string):
+
         current_value = values[roman_string[i]]
 
         if i + 1 < len(roman_string):
