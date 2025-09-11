eac40044ee111babc4874a81bc138171a330d23f
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-11 14:47:22 +0200

    fix(Python - Exceptions): :bug:

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,10 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count = count + 1 
-        except  (TypeError, ValueError):
-            pass
-        except IndexError:
-            break     
+        except (ValueError, TypeError):
+            continue
         
     print("")
     return count
