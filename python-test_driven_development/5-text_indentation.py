44d08d878b5f681821e52cea9c474974b7bf2d2d
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-13 10:42:28 +0200

    docs(Python - Test-driven development): :hammer:

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
+"""
+Module qui fournit une fonction text_indentation
+Cette fonction permet de couper le texte
+"""
+
+
 def text_indentation(text):
+    """
+    Imprime un texte avec deux sauts de ligne après
+    chaque '.', '?', ':'
+    """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
