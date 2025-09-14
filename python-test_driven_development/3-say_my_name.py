47c1bf43f848cd2678209af20004ecd4c2c22b30
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-14 18:05:16 +0200

    fix(Python - Test-driven development): :bug: Pycodestyle validation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-   fonction that print my name 
+   fonction that print my name
 """
+
 
 def say_my_name(first_name, last_name=""):
     """
@@ -16,10 +17,10 @@ def say_my_name(first_name, last_name=""):
         TypeError: first_name must be a string
         TypeError: last_name must be a string
     """
-    
-    if not isinstance(first_name, str ):
+
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str ):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print(f"My name is {first_name} {last_name}")
