683a89f2f688a585ba025a04f1ca83d694f0520e
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-14 19:39:55 +0200

    fix(Python - More Data Structures: Set, Dictionary): :bug:

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
 """
-function that returns the weighted average of all integers tuple (<score>, <weight>)
+function that returns the weighted 
+average of all integers tuple (<score>, <weight>)
 """
+
 
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    
+
     total_score = 0
     total_weight = 0
-    
+
     for score, weight  in my_list:
         total_score += score * weight
         total_weight += weight
-        
+
     return total_score / total_weight
