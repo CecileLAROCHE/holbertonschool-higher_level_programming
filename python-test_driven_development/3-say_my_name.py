8121e99518d564d12016d537ea02778b85f77c63
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-14 17:46:14 +0200

    docs(Python - Test-driven development):

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 
+"""
+   fonction that print my name 
+"""
+
 def say_my_name(first_name, last_name=""):
+    """
+    Say_my_name
+
+    Args:
+        first_name (_type_): _description_
+        last_name (str, optional): _description_. Defaults to "".
+
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
+    
     if not isinstance(first_name, str ):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str ):
         raise TypeError("last_name must be a string")
-    
     
     print (f"My name is {first_name} {last_name}")
     
