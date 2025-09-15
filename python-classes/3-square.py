4cfcb201a8e3402bd6b1657ad9b62925c80524e4
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-15 16:17:33 +0200

    build(Python - Classes and Objects): :hammer: task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""""
+this module do a class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """
+    This class defines a square based on it's private attribute
+    size. Size is private to ensure better control of the square.
+
+    Args:
+        size: The size of the square
+
+    Attributes:
+        size: The size of the square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+        
+        
+    def area(self):
+        return self.__size ** 2
+        
