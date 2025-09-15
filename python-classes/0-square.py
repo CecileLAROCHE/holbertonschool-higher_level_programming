a6f973b4391193f66a5102fd82a4c8b3079634f1
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-15 15:07:48 +0200

    fix(Python - Classes and Objects): :bug:

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,10 +7,12 @@ this module do an empty class Square that defines a square:
 
 class Square:
     """
-    Classe représentant un carré.
+        This is a blueprint of a class that defines a square.
 
-    Pour l'instant, cette classe est vide et ne contient aucun attribut ni méthode.
-    Elle peut être étendue ultérieurement pour inclure des propriétés comme la taille,
-    l'aire ou le périmètre d'un carré.
+    Args:
+        None
+
+    Attributes:
+        None
     """
     pass
