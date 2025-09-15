251a607a73397dd1951f9fdc33e65b223eafdefb
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-15 15:06:30 +0200

    docs(Python - Classes and Objects): :pencil2: task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,2 +1,16 @@
+#!/usr/bin/python3
+
+""""
+this module do an empty class Square that defines a square:
+"""
+
+
 class Square:
+    """
+    Classe représentant un carré.
+
+    Pour l'instant, cette classe est vide et ne contient aucun attribut ni méthode.
+    Elle peut être étendue ultérieurement pour inclure des propriétés comme la taille,
+    l'aire ou le périmètre d'un carré.
+    """
     pass
