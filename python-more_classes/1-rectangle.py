9247169c8d382ca03f28db259c38c0e8938629c6
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-16 12:30:47 +0200

    build(Python - More Classes and Objects): :hammer:

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle
+"""
+
+
+class Rectangle:
+    """
+        This is a Rectangle
+
+    Args:
+        width
+        height
+
+    Attributes:
+        None
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter : vérifie et assigne la width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter : vérifie et assigne la height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
