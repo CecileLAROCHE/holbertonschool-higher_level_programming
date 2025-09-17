2be5c72bcb02ff4aa6fedf372144ab5b8dfe96b5
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-17 15:48:38 +0200

    build(Python - Classes and Objects): :hammer: task 9

---
+++ python-classes/102-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+this module do a class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """
+    This class defines a square with a private attribute __size.
+
+    Args:
+        size (int): The size of the square's side (default is 0).
+
+    Attributes:
+        __size (int): Size of the square's side (private)
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter : retourne la taille du carré"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter : vérifie et assigne la taille"""
+        if not isinstance(value, int, float):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """== based on area"""
+        pass
+
+    def __ne__(self, other):
+        """!= based on area"""
+        pass
+
+    def __lt__(self, other):
+        """< based on area"""
+        pass
+
+    def __le__(self, other):
+        """<= based on area"""
+        pass
+
+    def __gt__(self, other):
+        """> based on area"""
+        pass
+
+    def __ge__(self, other):
+        """>= based on area"""
+        pass
