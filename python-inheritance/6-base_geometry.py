075db8ffd668e7121f95a4b15d0bda0c4fafa6a2
CecileLAROCHE <11458@holbertonstudents.com>
2025-09-23 15:59:05 +0200

    build(Python - Inheritance): :hammer: task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this module do an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+
+    """
+        do an empty class BaseGeometry
+
+    Args:
+        None
+
+    Attributes:
+        None
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
