fefb11f734cd9bf74c370b7fc3967699d7c18c04
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-29 21:54:45 +0200

    ajout de commentaires

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-
+Module contenant une fonction pour lire un fichier texte en UTF-8
+et afficher son contenu sur la sortie standard.
 """
 
 
 def read_file(filename=""):
+    """Lit un fichier texte UTF-8 et affiche son contenu sur stdout"""
     with open(filename, 'r', encoding="utf-8") as f:
         contenu = f.read()
         print(contenu, end="")
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """
-
+Module contenant une fonction pour écrire une chaîne de caractères
+dans un fichier texte en UTF-8.
 """
 
 
 def write_file(filename="", text=""):
+    """Écrit une chaîne dans un fichier texte UTF-8.
+    Écrase le contenu du fichier s'il existe déjà
+    et retourne le nombre de caractères écrits.
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         nb_caracteres = f.write(text)
         return nb_caracteres
