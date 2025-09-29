9abd6c2bb2f3e337987f0af7ab9388246eb8c07f
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-09-30 01:13:42 +0200

    fin de journée

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module contenant une fonction pour écrire une chaîne de caractères
+à la fin d'un fichier texte en UTF-8.
+"""
+json.dumps()
+
+
+def to_json_string(my_obj):
