adfbd64f154b8a7ed8237bb5148d95512c7da25a
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-07 14:59:34 +0200

    build(RESTful API): :hammer:

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 """
-*****
+Module that interacts with the JSONPlaceholder API.
+
+Contains two functions:
+- fetch_and_print_posts(): fetches posts and prints their titles.
+- fetch_and_save_posts(): fetches posts and saves them into a CSV file.
 """
 
-fetch_and_print_posts()
+import requests
+import csv
 
 
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("Status Code:", response.status_code)
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(f"{post['title']}")
 
 
-fetch_and_save_posts()
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        filtered_posts = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in posts
+        ]
+        with open("posts.csv", "w", encoding="utf-8", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(filtered_posts)
+    print("Status Code:", response.status_code)
