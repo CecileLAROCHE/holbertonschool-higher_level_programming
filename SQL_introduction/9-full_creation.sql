7082a328a114254e3a0dbeaaa11082ffc35c071f
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-10-20 22:13:59 +0200

    tach 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,13 @@
--- This script will create second_table and insert multiple rows
-CREATE TABLE IF NOT EXISTS Second_table (
+-- This script creates the table second_table and inserts multiple rows
+
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
-	score INT
+    score INT
 );
 
-INSERT into Second_table (id, name, score)
-VALUES
+INSERT INTO second_table (id, name, score)
+VALUES 
     (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
