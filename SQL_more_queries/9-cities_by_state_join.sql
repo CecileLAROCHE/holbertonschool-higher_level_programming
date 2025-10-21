bc105ea029664e5ffe8dbec7e6f639490fe58de5
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-21 16:33:03 +0200

    correction 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,6 @@
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states 
+    ON cities.state_id = states.id
 ORDER BY cities.id ASC;
