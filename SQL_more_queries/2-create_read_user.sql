56662ed1eb0623bf01d6670a79ca810114ae7246
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-21 13:59:27 +0200

    correction task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create users read only
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT READ ON *.* TO 'user_0d_2'@'localhost';
+GRANT READ ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
