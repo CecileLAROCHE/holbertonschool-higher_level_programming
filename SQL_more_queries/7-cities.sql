b98a763fc7886aaad560b7ecabd446222364bda2
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-21 15:56:16 +0200

    invertion des lignes

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities  (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     states(id) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id) NOT NULL,
     name VARCHAR(256) NOT NULL
+    FOREIGN KEY (state_id) REFERENCES states(id) NOT NULL,
 );
