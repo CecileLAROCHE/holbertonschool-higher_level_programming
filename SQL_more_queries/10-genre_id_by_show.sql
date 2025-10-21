f6d1b9e9219a0c040b3d38692b18a86bd89d20c2
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-21 17:06:26 +0200

    test

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,5 @@
----Script that gives the genre ids of each shows
-
-SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
-FROM `tv_shows`
-JOIN `tv_show_genres`
-    ON `tv_shows`.`id` = `tv_show_genres`.`tv_show_id`
-ORDER BY `tv_shows`.`title` ASC, `tv_show_genres`.`genre_id` ASC;
+--List all show
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
