f4ff3e3108b55279f411a9136115abf60d0d4db8
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-21 15:19:22 +0200

    oubli de ";"

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- This script will create database
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states  (
