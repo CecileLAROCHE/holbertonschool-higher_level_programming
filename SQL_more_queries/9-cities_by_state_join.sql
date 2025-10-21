ef41c1336eb323b1f135ff567003b4fb0b0d5062
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-21 16:50:51 +0200

    task 10

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 --- script that list all cities with states name
 
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states 
-    ON states.id = cities.state_id
-ORDER BY cities.id ASC;
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
+FROM `cities`
+INNER JOIN `states`
+    ON `states`.`id` = `cities`.`state_id`
+ORDER BY `cities`.`id` ASC;
