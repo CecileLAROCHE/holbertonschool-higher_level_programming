63fde29e937f3f5a65a23152080e5d15faf55dff
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-21 16:19:46 +0200

    task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,11 @@
 -- This script will create database and table
 
 USE hbtn_0d_usa;
-    SELECT name 
-    FROM cities
-    WHERE state_id = 
-        (SELECT id 
-        FROM states 
-        WHERE name = 'California
-        ')
-    ORDER BY id ASC;
+SELECT name 
+FROM cities
+WHERE state_id = 
+    (SELECT id 
+    FROM states 
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
