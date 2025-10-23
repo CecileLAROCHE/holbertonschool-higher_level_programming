18311e16427dad2650f21dcad804290f900097ee
CecileLAROCHE <11458@holbertonstudents.com>
2025-10-23 12:24:00 +0200

    tesk 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Script qui se connecte à une base MySQL et affiche toutes les lignes
+de la table 'states' dont le nom commence par 'N', triées par id croissant.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Récupération des arguments : username, password, database
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connexion à la base de données MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+
+    # Création du curseur
+    cursor = db.cursor()
+
+    # Exécution de la requête pour récupérer tous les états commençant
+    # par N triés par id
+    query = (
+        "SELECT id, name FROM states "
+        "WHERE name LIKE BINARY '{}' "
+        "ORDER BY id ASC;"
+    ).format(state_name)
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    # Affichage des résultats
+    for row in rows:
+        print(row)
+
+    # Fermeture du curseur et de la connexion
+    cursor.close()
+    db.close()
