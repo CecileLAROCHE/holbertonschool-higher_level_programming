a0c20550809aca98b1e23c0a23e4de80a51939eb
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-10-26 16:19:05 +0100

    task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+...
+"""
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Récupération des arguments : username, password, database
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Créer l’engine avec ces valeurs
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost/{database}",
+        pool_pre_ping=True)
+
+    # Créer la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Faire la requête + afficher
+    state = session.query(State).filter_by(id=2).first()
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    # Fermer la session
+    session.close()
